ab33143372a160dd4cf00947160cf8c6b91c8df2
jiaquan.dong <jiaquan.dong@thomsonreuters.com>
2017-12-01 14:01:15 +0800

    update xgbranker_example.py

--- python-package/example/xgbranker_example.py
+++ python-package/example/xgbranker_example.py
@@ -10,7 +10,10 @@ y = np.random.randint(5, size=case_num)
 print "X=", X
 print "y=", y
 
-ranker = XGBRanker(n_estimators=150, learning_rate=0.1, subsample=0.9)
+# objective = rank:pairwise(default).
+# Although rank:ndcg is also available,  rank:ndcg(listwise) is much worse than pairwise.
+# So ojective is always rank:pairwise whatever you write. 
+ranker = XGBRanker(n_estimators=150, learning_rate=0.1, subsample=0.9)#, objective='rank:pairwise')
 # 100 samples, 4 groups, 25/group, predicted_values can be used to sort x in their own group
 ranker.fit(X, y, [25, 25, 25, 25], eval_metric=['ndcg', 'map@5-'])
 y_predict = ranker.predict(X, [25, 25, 25, 25])
