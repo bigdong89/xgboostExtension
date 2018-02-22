227ab78e43a11fbb52064f0bf6c2df2c55a47b15
bigdong <dongjiaquan89@gmail.com>
2018-02-22 23:59:32 +0800

    change xgbranker_example to help understand better

--- example/xgbranker_example.py
+++ example/xgbranker_example.py
@@ -1,7 +1,7 @@
 import numpy as np
 from xgboostextension import XGBRanker
 
-case_num = 100
+case_num = 20
 X = np.random.rand(case_num, 4)
 y = np.random.randint(5, size=case_num)
 print("X="+str(X))
@@ -12,9 +12,9 @@ print("y="+str(y))
 # So ojective is always rank:pairwise whatever you write. 
 ranker = XGBRanker(n_estimators=150, learning_rate=0.1, subsample=0.9)#, objective='rank:pairwise')
 # 100 samples, 4 groups, 25/group, predicted_values can be used to sort x in their own group
-ranker.fit(X, y, [25, 25, 25, 25], eval_metric=['ndcg', 'map@5-'])
-y_predict = ranker.predict(X, [25, 25, 25, 25])
+ranker.fit(X, y, [5, 5, 5, 5], eval_metric=['ndcg', 'map@5-'])
+y_predict = ranker.predict(X, [5, 5, 5, 5])
 
 print("predict:"+str(y_predict))
-print("type(y_predict):"+type(y_predict))
+print("type(y_predict):"+str(type(y_predict)))
 
