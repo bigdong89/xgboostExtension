ea8ec55f64523f688fbf5a9a10c12f61429e3a83
Floris Hoogenboom <floris@digitaldreamworks.nl>
2018-04-24 13:41:12 +0200

    Updated example to comply with new way of specifying groups.

--- example/xgbranker_example.py
+++ example/xgbranker_example.py
@@ -1,19 +1,34 @@
 import numpy as np
 from xgboostextension import XGBRanker
 
-case_num = 20
-X = np.random.rand(case_num, 4)
-y = np.random.randint(5, size=case_num)
-print("X="+str(X))
+CASE_NUM = 20
+GROUPS_NUM = 4
+
+if CASE_NUM % GROUPS_NUM != 0:
+    raise ValueError('Cases should be splittable into equal groups.')
+
+# Generate some sample data to illustrate ranking
+X_features = np.random.rand(CASE_NUM, 4)
+y = np.random.randint(5, size=CASE_NUM)
+X_groups = np.arange(0, GROUPS_NUM).repeat(CASE_NUM/GROUPS_NUM)
+
+print("X="+str(X_features))
 print("y="+str(y))
+
+# Append the group labels as a first axis to the features matrix
+# this is how the algorithm can distinguish between the different
+# groups
+X = np.concatenate([X_groups[:,None], X_features], axis=1)
+
 
 # objective = rank:pairwise(default).
 # Although rank:ndcg is also available,  rank:ndcg(listwise) is much worse than pairwise.
 # So ojective is always rank:pairwise whatever you write. 
-ranker = XGBRanker(n_estimators=150, learning_rate=0.1, subsample=0.9)#, objective='rank:pairwise')
-# 100 samples, 4 groups, 25/group, predicted_values can be used to sort x in their own group
-ranker.fit(X, y, [5, 5, 5, 5], eval_metric=['ndcg', 'map@5-'])
-y_predict = ranker.predict(X, [5, 5, 5, 5])
+ranker = XGBRanker(n_estimators=150, learning_rate=0.1, subsample=0.9)
+
+
+ranker.fit(X, y, eval_metric=['ndcg', 'map@5-'])
+y_predict = ranker.predict(X)
 
 print("predict:"+str(y_predict))
 print("type(y_predict):"+str(type(y_predict)))
